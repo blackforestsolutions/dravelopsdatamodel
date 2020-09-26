0fff0f77a5562d3b8ff432515dcc3743769067a8
jobdk <47722294+jobdk@users.noreply.github.com>
2020-09-26 16:37:47 +0200

    initial datamodel ApiToken implementation

---
+++ src/main/java/de/blackforestsolutions/dravelopsdatamodel/ApiToken.java
@@ -0,0 +1,24 @@
+package de.blackforestsolutions.dravelopsdatamodel;
+
+import java.io.Serializable;
+import java.time.ZonedDateTime;
+import java.util.Locale;
+
+public class ApiToken implements Serializable {
+    private final String host;
+    private final String protocol;
+    private final Integer port;
+    private final String departure;
+    private final String arrival;
+    private final ZonedDateTime departureDate;
+    private final ZonedDateTime arrivalDate;
+    private final boolean isDeparture;
+    private final Locale locale;
+    private final Optimization optimization;
+    private final String router;
+    private final
+    private final
+    private final
+    private final
+    private final
+}
