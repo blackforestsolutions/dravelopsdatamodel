49fa0f69f40f0d3bb2dd55c3ed910688617d42b1
hennihaus <jan-hendrik.hausner@outlook.com>
2020-10-05 16:29:22 +0200

    added exception class

---
+++ src/main/java/de/blackforestsolutions/dravelopsdatamodel/exception/NoExternalResultFoundException.java
@@ -0,0 +1,8 @@
+package de.blackforestsolutions.dravelopsdatamodel.exception;
+
+public class NoExternalResultFoundException extends Exception {
+
+    public NoExternalResultFoundException() {
+        super("Logical error triggered by API");
+    }
+}
